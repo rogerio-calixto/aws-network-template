10b93fe3326c38ac4d1bab9c0669d4648db37fec
Rogerio Calixto <107159671+rogerio-calixto@users.noreply.github.com>
2023-12-12 16:16:26 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,12 @@ variable "aws_profile" {
   type = string
   default = null
 }
+variable "aws_access_key" {
+  type = string
+}
+variable "aws_access_secret_key" {
+  type = string
+}
 variable "aws_region" {}
 variable "project" {}
 variable "environment" {}
